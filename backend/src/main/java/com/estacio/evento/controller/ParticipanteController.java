d274c19baca71a7ff0a2555773c25deeb9106542
Matheus Magalhães <matheus.magalha95@gmail.com>
2022-10-06 00:25:59 -0300

    implementando endpoint autenticar

--- backend/src/main/java/com/estacio/evento/controller/ParticipanteController.java
+++ backend/src/main/java/com/estacio/evento/controller/ParticipanteController.java
@@ -1,7 +1,9 @@
 package com.estacio.evento.controller;
 
+import com.estacio.evento.exception.RegraNegocioException;
 import com.estacio.evento.model.Participante;
 import com.estacio.evento.service.ParticipanteService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -34,6 +36,18 @@ public class ParticipanteController {
     @PostMapping
     public ResponseEntity<Object> save(@RequestBody Participante participante) {
         return ResponseEntity.status(HttpStatus.CREATED).body(participanteService.salvarParticipante(participante));
+    }
+
+    @PostMapping("/autenticar")
+    public ResponseEntity<String> autenticar(@RequestBody Participante participante) {
+        try {
+            if (participanteService.autenticar(participante.getEmail(), participante.getSenha())) {
+                return ResponseEntity.status(HttpStatus.OK).body("Participante autenticado com sucesso!");
+            }
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+        return ResponseEntity.badRequest().build(); // retorna apenas um badrequest sem msg
     }
 
     @DeleteMapping(value = "/{id}")
